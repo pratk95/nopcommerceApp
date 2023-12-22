5693d6fc3a78c741c993b0e302467a8d5073666b
pratk95 <prateek95gupta@gmail.com>
2023-12-22 13:31:54 +0530

    run.bat update.....

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 REM pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
 REM pytest -s -v -m "sanity or regressoin" --html=./Reports/report.html testCases/ --browser chrome
 REM pytest -s -v -m "sanity and regression" --html=./Reports/report.html testCases/ --browser chrome
-pytest -s -v -m "regression" --html./Reports/report.html testCases/ --browser chrome
+pytest -s -v -m "regression" --html=./Reports/report.html testCases/ --browser chrome
